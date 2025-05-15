620e0628b58a600c26ecd2ba7d85f510c8351be6
SteinerTheoEPSIC <steinertheo@icloud.com>
2025-05-15 22:00:24 +0000

    Modif du message Flask pour test Jenkins

--- app-TonNom.py
+++ app-TonNom.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return '✅ Jenkins à déployé cette version automatiquement!'
+        return '✅ Jenkins fonctionne parfaitement bien lagona !'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
